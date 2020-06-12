e202a1879eb6fff527da5973b3a2258a720be7d3
kantegory <davidobryakov@gmail.com>
2020-06-12 19:59:55 +0300

    Add all endpoints

--- django-freelance/django_freelance/freelance/urls.py
+++ django-freelance/django_freelance/freelance/urls.py
@@ -3,6 +3,54 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('executors/<int:pk>', ExecutorRetrieveView.as_view()),
+    path('executors/update/<int:pk>', ExecutorUpdateView.as_view()),
+    path('executors/all', ExecutorListView.as_view()),
+    path('executors/new', ExecutorCreateView.as_view()),
+
+    path('customers/<int:pk>', CustomerRetrieveView.as_view()),
+    path('customers/update/<int:pk>', CustomerUpdateView.as_view()),
+    path('customers/all', CustomerListView.as_view()),
+    path('customers/new', CustomerCreateView.as_view()),
+
+    path('orders/<int:pk>', OrderRetrieveView.as_view()),
+    path('orders/update/<int:pk>', OrderUpdateView.as_view()),
+    path('orders/all', OrderListView.as_view()),
+    path('orders/new', OrderCreateView.as_view()),
+
+    path('services/<int:pk>', ServiceRetrieveView.as_view()),
+    path('services/update/<int:pk>', ServiceUpdateView.as_view()),
+    path('services/all', ServiceListView.as_view()),
+    path('services/new', ServiceCreateView.as_view()),
+
+    path('tags/<int:pk>', TagRetrieveView.as_view()),
+    path('tags/update/<int:pk>', TagUpdateView.as_view()),
+    path('tags/all', TagListView.as_view()),
+    path('tags/new', TagCreateView.as_view()),
+
+    path('orderings/<int:pk>', OrderingRetrieveView.as_view()),
+    path('orderings/update/<int:pk>', OrderingUpdateView.as_view()),
+    path('orderings/all', OrderingListView.as_view()),
+    path('orderings/new', OrderingCreateView.as_view()),
+
+    path('messages/<int:pk>', MessageRetrieveView.as_view()),
+    path('messages/update/<int:pk>', MessageUpdateView.as_view()),
+    path('messages/all', MessageListView.as_view()),
+    path('messages/new', MessageCreateView.as_view()),
+
+    path('tickets/<int:pk>', TicketRetrieveView.as_view()),
+    path('tickets/update/<int:pk>', TicketUpdateView.as_view()),
     path('tickets/all', TicketListView.as_view()),
+    path('tickets/new', TicketCreateView.as_view()),
+
+    path('reviews/<int:pk>', ReviewRetrieveUpdateView.as_view()),
+    path('reviews/update/<int:pk>', ReviewRetrieveUpdateView.as_view()),
+    path('reviews/all', ReviewListView.as_view()),
+    path('reviews/new', ReviewCreateView.as_view()),
+
+    path('authorings/<int:pk>', AuthoringRetrieveView.as_view()),
+    path('authorings/update/<int:pk>', AuthoringUpdateView.as_view()),
+    path('authorings/all', AuthoringListView.as_view()),
+    path('authorings/new', AuthoringCreateView.as_view()),
 ]
 
--- django-freelance/django_freelance/freelance/views.py
+++ django-freelance/django_freelance/freelance/views.py
@@ -1,11 +1,265 @@
-from rest_framework.response import Response
 from rest_framework import generics
 
 from .models import *
 from .serializers import *
 
 
+class ExecutorRetrieveView(generics.RetrieveAPIView):
+    queryset = Executor.objects.all()
+    serializer_class = ExecutorSerializer
+
+
+class ExecutorUpdateView(generics.UpdateAPIView):
+    queryset = Executor.objects.all()
+    serializer_class = CreateExecutorSerializer
+
+
+class ExecutorCreateView(generics.CreateAPIView):
+    queryset = Executor.objects.all()
+    serializer_class = CreateExecutorSerializer
+
+
+class ExecutorListView(generics.ListAPIView):
+    queryset = Executor.objects.all()
+    serializer_class = ExecutorSerializer
+
+
+class CustomerRetrieveView(generics.RetrieveAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerUpdateView(generics.UpdateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CreateCustomerSerializer
+
+
+class CustomerCreateView(generics.CreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CreateCustomerSerializer
+
+
+class CustomerListView(generics.ListAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class OrderRetrieveView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderUpdateView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = CreateOrderSerializer
+
+
+class OrderCreateView(generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = CreateOrderSerializer
+
+
+class OrderListView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        queryset = Order.objects.all()
+
+        params = self.request.query_params
+
+        service_type = params.get('service', None)
+        price = params.get('price', None)
+        customer = params.get('customer', None)
+
+        if service_type:
+            queryset = queryset.filter(service_type=service_type)
+
+        if price:
+            queryset = queryset.filter(price__lte=price)
+
+        if customer:
+            queryset = queryset.filter(customer__id=customer)
+
+        return queryset
+
+
+class ServiceRetrieveView(generics.RetrieveAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class ServiceUpdateView(generics.UpdateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = CreateServiceSerializer
+
+
+class ServiceCreateView(generics.CreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = CreateServiceSerializer
+
+
+class ServiceListView(generics.ListAPIView):
+    serializer_class = ServiceSerializer
+
+    def get_queryset(self):
+        queryset = Service.objects.all()
+
+        params = self.request.query_params
+
+        service_type = params.get('service', None)
+        price = params.get('price', None)
+        executor = params.get('executor', None)
+
+        if service_type:
+            queryset = queryset.filter(service_type=service_type)
+
+        if price:
+            queryset = queryset.filter(price__lte=price)
+
+        if executor:
+            queryset = queryset.filter(executor__id=executor)
+
+        return queryset
+
+
+class TagRetrieveView(generics.RetrieveAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class TagUpdateView(generics.UpdateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = CreateTagSerializer
+
+
+class TagCreateView(generics.CreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = CreateTagSerializer
+
+
+class TagListView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class OrderingRetrieveView(generics.RetrieveAPIView):
+    queryset = Ordering.objects.all()
+    serializer_class = OrderingSerializer
+
+
+class OrderingUpdateView(generics.UpdateAPIView):
+    queryset = Ordering.objects.all()
+    serializer_class = CreateOrderingSerializer
+
+
+class OrderingCreateView(generics.CreateAPIView):
+    queryset = Ordering.objects.all()
+    serializer_class = CreateOrderingSerializer
+
+
+class OrderingListView(generics.ListAPIView):
+    queryset = Ordering.objects.all()
+    serializer_class = OrderingSerializer
+
+
+class MessageRetrieveView(generics.RetrieveAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+class MessageUpdateView(generics.UpdateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = CreateMessageSerializer
+
+
+class MessageCreateView(generics.CreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = CreateMessageSerializer
+
+
+class MessageListView(generics.ListAPIView):
+    #queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        queryset = Message.objects.all()
+
+        params = self.request.query_params
+
+        executor = params.get('executor', None)
+        customer = params.get('customer', None)
+        from_date = params.get('from_date', None)
+        to_date = params.get('to_date', None)
+
+        if executor:
+            queryset = queryset.filter(executor__id=executor)
+
+        if customer:
+            queryset = queryset.filter(customer__id=customer)
+
+        if from_date:
+            queryset = queryset.filter(msg_date__gte=from_date)
+
+        if to_date:
+            queryset = queryset.filter(msg_date__lte=to_date)
+
+        return queryset
+
+
+class TicketRetrieveView(generics.RetrieveAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class TicketUpdateView(generics.UpdateAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = CreateTicketSerializer
+
+
+class TicketCreateView(generics.CreateAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = CreateTicketSerializer
+
+
 class TicketListView(generics.ListAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+
+class ReviewRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewCreateView(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewListView(generics.ListAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class AuthoringRetrieveView(generics.RetrieveAPIView):
+    queryset = Authoring.objects.all()
+    serializer_class = AuthoringSerializer
+
+
+class AuthoringUpdateView(generics.UpdateAPIView):
+    queryset = Authoring.objects.all()
+    serializer_class = CreateAuthoringSerializer
+
+
+class AuthoringCreateView(generics.CreateAPIView):
+    queryset = Authoring.objects.all()
+    serializer_class = CreateAuthoringSerializer
+
+
+class AuthoringListView(generics.ListAPIView):
+    queryset = Authoring.objects.all()
+    serializer_class = AuthoringSerializer
+
+
+
 
