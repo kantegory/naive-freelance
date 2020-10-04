6a88a20bb0def81cd6f2fb6b32a4f7bd1f9d51db
kantegory <davidobryakov@gmail.com>
2020-10-04 16:14:52 +0300

    Refactor: autocomplete off

--- vue-freelance/src/components/auth/SignUp.vue
+++ vue-freelance/src/components/auth/SignUp.vue
@@ -3,11 +3,11 @@
     <b-form @submit="register">
       <div class="form-group">
         <label for="username">Логин:</label>
-        <b-input v-model="username" type="text" id="username" placeholder="Логин..."></b-input>
+        <b-input v-model="username" type="text" id="username" placeholder="Логин..." autocomplete="off"></b-input>
       </div>
       <div class="form-group">
         <label for="phone">Номер телефона:</label>
-        <b-input v-model="phone" type="text" id="phone" placeholder="+7 (921) 123 45 67"></b-input>
+        <b-input v-model="phone" type="text" id="phone" placeholder="+7 (921) 123 45 67" autocomplete="off"></b-input>
       </div>
       <div class="form-group">
         <label for="customerOrExecutor">Вы заказчик или исполнитель?</label>
@@ -16,11 +16,11 @@
       </div>
       <div class="form-group">
         <label for="password">Пароль:</label>
-        <b-input v-model="password" type="password" id="password" placeholder="Пароль..."></b-input>
+        <b-input v-model="password" type="password" id="password" placeholder="Пароль..." autocomplete="off"></b-input>
       </div>
       <div class="form-group">
         <label for="repeatPassword">Повторите пароль:</label>
-        <b-input v-model="repeatPassword" type="password" id="repeatPassword" placeholder="Повторите пароль..."></b-input>
+        <b-input v-model="repeatPassword" type="password" id="repeatPassword" placeholder="Повторите пароль..." autocomplete="off"></b-input>
       </div>
       <b-button variant="primary" type="submit">Регистрация</b-button>
       <p class="mt-3">Уже есть аккаунт? <router-link to="/auth/signin">Вход</router-link>
